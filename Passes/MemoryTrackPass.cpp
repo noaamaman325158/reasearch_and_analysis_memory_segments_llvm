eaa3dd9a86e2d43d84ad46ae34801bcdc0033a1d
noaa <noaamaman325158@gmail.com>
2024-02-02 23:16:32 +0200

    Add soem postgres db for track the pointers realted data

--- Passes/MemoryTrackPass.cpp
+++ Passes/MemoryTrackPass.cpp
@@ -2,39 +2,83 @@
 #include "llvm/IR/Function.h"
 #include "llvm/IR/Instruction.h"
 #include "llvm/IR/Instructions.h"
+#include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/Module.h"
-#include "llvm/IR/Type.h"
+#include "llvm/IR/DataLayout.h"
 #include "llvm/Support/raw_ostream.h"
+#include "pqxx/pqxx"
 
 using namespace llvm;
+using namespace pqxx;
 
 namespace {
-  struct MemoryTrackerPass : public FunctionPass {
-    static char ID;
-    MemoryTrackerPass() : FunctionPass(ID) {}
+    struct MemoryTrackerPass : public FunctionPass {
+        static char ID;
+        connection C;  // Database connection object
 
-    virtual bool runOnFunction(Function &F) override {
-      const DataLayout &DL = F.getParent()->getDataLayout();
+        MemoryTrackerPass() : FunctionPass(ID), C("dbname=analysis_system_info user=myuser password=mypassword") {}
 
-      for (auto &B : F) {
-        for (auto &I : B) {
-          if (auto *loadInst = dyn_cast<LoadInst>(&I)) {
-            // Handle load instruction
-            Value *ptr = loadInst->getPointerOperand();
-            unsigned size = DL.getTypeAllocSize(ptr->getType()->getPointerElementType());
-            errs() << "Load Instruction: Ptr = " << *ptr << ", Size = " << size << "\n";
-          } else if (auto *storeInst = dyn_cast<StoreInst>(&I)) {
-            // Handle store instruction
-            Value *ptr = storeInst->getPointerOperand();
-            unsigned size = DL.getTypeAllocSize(ptr->getType()->getPointerElementType());
-            errs() << "Store Instruction: Ptr = " << *ptr << ", Size = " << size << "\n";
-          }
+        virtual bool runOnFunction(Function &F) override {
+            Module *M = F.getParent();
+            LLVMContext &Context = M->getContext();
+            const DataLayout &DL = M->getDataLayout();
+
+            // Define the prototype of the logging function.
+            FunctionType *logFuncType = FunctionType::get(Type::getVoidTy(Context), false);
+            FunctionCallee logFunc = M->getOrInsertFunction("logMemoryAccess", logFuncType);
+
+            for (auto &B : F) {
+                for (auto &I : B) {
+                    if (auto *loadInst = dyn_cast<LoadInst>(&I)) {
+                        // Handle load instruction
+                        handleMemoryAccess(loadInst, DL, Context, logFunc, "load");
+                    } else if (auto *storeInst = dyn_cast<StoreInst>(&I)) {
+                        // Handle store instruction
+                        handleMemoryAccess(storeInst, DL, Context, logFunc, "store");
+                    }
+                }
+            }
+
+            return false;
         }
-      }
-      return false;
-    }
-  };
+
+        void handleMemoryAccess(Instruction *inst, const DataLayout &DL, LLVMContext &Context, FunctionCallee logFunc, const std::string &operationType) {
+            IRBuilder<> Builder(inst);
+            Builder.CreateCall(logFunc);  
+
+            Value *ptr = nullptr;
+            if (auto *loadInst = dyn_cast<LoadInst>(inst)) {
+                ptr = loadInst->getPointerOperand();
+            } else if (auto *storeInst = dyn_cast<StoreInst>(inst)) {
+                ptr = storeInst->getPointerOperand();
+            }
+
+            if (ptr) {
+                unsigned size = DL.getTypeAllocSize(ptr->getType()->getPointerElementType());
+
+                // Format the pointer address as a string
+                std::string ptrAddress = ptr->getName().str();
+                if (ptrAddress.empty()) {
+                    ptrAddress = "anonymous";
+                }
+
+                // SQL query
+                std::string query = "INSERT INTO memory_operations (operation_type, pointer, size) VALUES ('" + operationType + "', '" + ptrAddress + "', " + std::to_string(size) + ");";
+
+                try {
+                    work W(C);
+                    W.exec(query);
+                    W.commit();
+                } catch (const std::exception &e) {
+                    errs() << "Database error: " << e.what() << "\n";
+                }
+            
+                errs() << "Memory Operation: Type = " << operationType << ", Ptr = " << ptrAddress << ", Size = " << size << "\n";
+            }
+        }
+    };
 }
 
 char MemoryTrackerPass::ID = 0;
+
 static RegisterPass<MemoryTrackerPass> X("memtrack", "Memory Tracking Pass", false, false);
